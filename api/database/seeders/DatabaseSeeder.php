1ccbccc54b95e94181a16e4e2a1506fb9dd3e65d
Mohamed Mutasim <falah.app@example.com>
2026-03-16 13:46:57 +0200

    Include all demo data in DatabaseSeeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ServiceSeeder::class,
             SocialLinkSeeder::class,
+            DemoDataSeeder::class,
         ]);
     }
 }
